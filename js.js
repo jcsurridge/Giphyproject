b3a03644ae315be50e59b921d2561f3bbef7a424
unknown <josh.c.surridge@gmail.com>
2019-11-18 21:11:25 -0700

    adding image movement functionality

--- js.js
+++ js.js
@@ -1,17 +1,45 @@
 $(document).ready(function () {
 
-    let animalArr = ["dog", "cat", "bird", "chipmunk", "whale"];
+    var animalArr = ["dog", "cat", "bird", "chipmunk", "whale"];
 
-    for (let i = 0; i < animalArr.length; i++) {
+    function initialButton() {
 
-        let addButton = $("<button>");
-        addButton.text(animalArr[i]);
-        $("#buttonRow").prepend(addButton);
-        addButton.attr("type-of-animal", animalArr[i]
-        )
+        for (let i = 0; i < animalArr.length; i++) {
+
+            var buttons = $("<button>");
+            buttons.text(animalArr[i]);
+            $("#buttonRow").append(buttons);
+            buttons.attr("type-of-animal", animalArr[i]);
+
+        }
+
     };
 
+    function newButton() {
+
+        $("#animal-button").on("click", function () {
+            event.preventDefault();
+
+            let newGiphy = $("#animal-input").val().trim();
+            let buttons = $("<button>");
+            buttons.text(newGiphy);
+            animalArr.push(newGiphy);
+            $("#buttonRow").append(buttons);
+            buttons.attr("type-of-animal", newGiphy);
+
+
+            console.log(animalArr);
+            console.log(newGiphy);
+
+        });
+    };
+
+
+    initialButton();
+    newButton();
+
     $("button").on("click", function () {
+
 
         $("#gifsappearhere").empty();
 
@@ -32,7 +60,7 @@ $(document).ready(function () {
 
                 let results = response.data;
 
-                for (let i = 0; i < 10; i++) {
+                for (let i = 0; i < animalArr.length; i++) {
 
                     let animalDiv = $("<div>");
                     let ratingDiv = $("<p>").text("Rating: " + results[i].rating);
@@ -47,6 +75,8 @@ $(document).ready(function () {
                     animalDiv.append(animalGiphy);
 
                     $("#gifsappearhere").prepend(animalDiv);
+
+                    newButton();
 
                 }
             });
@@ -65,18 +95,6 @@ $(document).ready(function () {
             $(this).attr("src", $(this).attr("data-still"));
             $(this).attr("data-state", "still");
         }
-    });
-
-$("#animal-button").on("click", function(){
-    event.preventDefault();
-
-    let newGiphy = $("#animal-input").val().trim();
-    console.log(newGiphy);
-
-
-
-
-
     });
 
 });
